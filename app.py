739ad83d51571f6b4281a0ca544374282dc21e03
amal70 <147558372+amal70@users.noreply.github.com>
2025-02-13 17:29:08 +0100

    app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ st.set_page_config(
 
 # 🌍 Barre latérale pour la navigation
 st.sidebar.title("📌 Navigation")
-st.sidebar.page_link("1_Accueil.py", label="🏠 Accueil")
-st.sidebar.page_link("2_Impact_Environnemental.py", label="📊 Impact Environnemental")
-st.sidebar.page_link("3_Benefices.py", label="🔍 Les Bénéfices du Vélo Mécanique")
-st.sidebar.page_link("4_Sensibilisation.py", label="🌍 Sensibilisation")
-st.sidebar.page_link("5_Passons_à_l'action.py", label="✅ Passons à l'action !")
+st.sidebar.page_link("pages/1_Accueil.py", label="🏠 Accueil")
+st.sidebar.page_link("pages/2_Impact_Environnemental.py", label="📊 Impact Environnemental")
+st.sidebar.page_link("pages/3_Benefices.py", label="🔍 Les Bénéfices du Vélo Mécanique")
+st.sidebar.page_link("pages/4_Sensibilisation.py", label="🌍 Sensibilisation")
+st.sidebar.page_link("pages/5_Passons_à_l'action.py", label="✅ Passons à l'action !")
 
 # 🎯 Page d'accueil (accueil par défaut)
 st.title("🚗➡️🚴‍♂️ Bienvenue sur Votre Impact Environnemental")
@@ -25,4 +25,4 @@ Utilisez la barre latérale pour naviguer entre les sections.
 
 # Bouton pour démarrer directement
 if st.button("Commencer l’analyse 🌱"):
-    st.switch_page("1_Accueil.py")  # Redirige vers la page d'accueil interactive
+    st.switch_page("pages/1_Accueil.py")  # Redirige vers la page d'accueil interactive
